32ab7740fc4a358f75d46049cb0a67491600adf0
Bryan Ralphs <38300718+bjralphs@users.noreply.github.com>
2022-08-30 12:47:45 -0600

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,8 +1,4 @@
-﻿/*
- * "I declare that the following source code was written solely by me. 
- * I understand that copying any source code, in whole or in part, constitutes cheating,
- * and that I will receive a zero on this project if I am found in violation of this policy.”
- */
+
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
